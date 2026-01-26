c64194eaa50fe4d5a4c5fc1bcdea4b98c28eec86
d33pan <mbank.deepan@gmail.com>
2026-01-26 22:49:07 +0545

    first commit

---
+++ src/main/java/com/cron_email/cron_email/CronEmailApplication.java
@@ -0,0 +1,13 @@
+package com.cron_email.cron_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cron_email/cron_email/CronEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cron_email.cron_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
