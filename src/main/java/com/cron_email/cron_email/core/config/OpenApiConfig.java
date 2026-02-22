b5709a6bd9cd6759c62abb0d5d59db8d72cc6df9
d33pan <mbank.deepan@gmail.com>
2026-02-22 23:28:42 +0545

    Add OpenAPI configuration with bearer token authentication setup
    - Implemented `OpenApiConfig` class to configure Swagger documentation.
    - Added JWT-based bearer authentication scheme for secure API documentation access.
    - Included metadata like API title, version, and description in the OpenAPI configuration.

---
+++ src/main/java/com/cron_email/cron_email/core/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.cron_email.cron_email.core.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author d33pan on 2/22/2026
+ */
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Your API")
+                        .version("1.0")
+                        .description("API documentation"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
